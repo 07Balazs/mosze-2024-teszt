b6c4174b15aa6bd5749ebfc45bafad0efd25f203
Razman Balazs <95970955+07Balazs@users.noreply.github.com>
2024-09-16 12:04:45 +0200

    hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // hiányzik a _
-    std::cout << '1-100 ertekek duplazasa' // '' helyett "" kell és hiányzik a ;
-    for (int i = 0;) // hiányos ciklusfelvétel
+    int *b = new int[N_ELEMENTS]; // hiányzik a _
+    std::cout << "1-100 ertekek duplazasa"<<std::endl; // '' helyett "" kell és hiányzik a ;
+    for (int i = 0; i < N_ELEMENTS; i++) // hiányos ciklusfelvétel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // hibás ciklusfelvétel
+    for (int i = 0; i<N_ELEMENTS; i++) // hibás ciklusfelvétel
     {
-        std::cout << "Ertek:" // hiányzi ka ; a sor végérõl, nem írja ki az értéket
+        std::cout << "Ertek:"<<b[i]<<std::endl; // hiányzi ka ; a sor végérõl, nem írja ki az értéket
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // double-nek kellene lennie hogy tizedes számot kapjunk az átlagnál
+    double atlag=0; // double-nek kellene lennie hogy tizedes számot kapjunk az átlagnál
     for (int i = 0; i < N_ELEMENTS, i++) // , helyett ;
     {
-        atlag += b[i] // hiányzik a ;
+        atlag += b[i]; // hiányzik a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b;
     return 0;
 }
