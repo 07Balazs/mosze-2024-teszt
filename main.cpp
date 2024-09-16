980fcb56dd1c0cddf1da398e2fe1c791956d2c6f
Razman Balazs <95970955+07Balazs@users.noreply.github.com>
2024-09-16 11:56:48 +0200

    Update main.cpp
    kommentelt hibák

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // hiányzik a _
+    std::cout << '1-100 ertekek duplazasa' // '' helyett "" kell és hiányzik a ;
+    for (int i = 0;) // hiányos ciklusfelvétel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // hibás ciklusfelvétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hiányzi ka ; a sor végérõl, nem írja ki az értéket
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // double-nek kellene lennie hogy tizedes számot kapjunk az átlagnál
+    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] // hiányzik a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
